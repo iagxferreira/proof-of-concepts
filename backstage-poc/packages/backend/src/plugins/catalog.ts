277348e078e13069a6dc79ef62e446b8eec00a21
Iago Ferreira <iago-ferreira@outlook.com>
2023-05-30 21:40:27 -0300

    feat: dockerizing dependencies to run backstage with ldap

--- backstage-poc/packages/backend/src/plugins/catalog.ts
+++ backstage-poc/packages/backend/src/plugins/catalog.ts
@@ -1,14 +1,52 @@
-import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
-import { Router } from 'express';
-import { PluginEnvironment } from '../types';
+import type {Router} from 'express';
+import type {PluginEnvironment} from '../types';
+
+import {CatalogBuilder} from '@backstage/plugin-catalog-backend';
+import {ScaffolderEntitiesProcessor} from '@backstage/plugin-scaffolder-backend';
+import {
+    LdapOrgEntityProvider as LDAPImmobiliare, LdapOrgReaderProcessor,
+} from '@backstage/plugin-catalog-backend-module-ldap';
+
+import { LdapOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-ldap';
+
 
 export default async function createPlugin(
-  env: PluginEnvironment,
+    env: PluginEnvironment,
 ): Promise<Router> {
-  const builder = await CatalogBuilder.create(env);
-  builder.addProcessor(new ScaffolderEntitiesProcessor());
-  const { processingEngine, router } = await builder.build();
-  await processingEngine.start();
-  return router;
+    const builder = await CatalogBuilder.create(env);
+
+    builder.addEntityProvider(
+        LdapOrgEntityProvider.fromConfig(env.config, {
+            id: 'admin',
+            target: 'ldap://localhost:1389',
+            logger: env.logger,
+            schedule: env.scheduler.createScheduledTaskRunner({
+                frequency: { minutes: 1 },
+                timeout: { minutes: 15 },
+            }),
+        }),
+    );
+
+    builder.addProcessor(
+        LdapOrgReaderProcessor.fromConfig(env.config, {
+            logger: env.logger,
+        }),
+    );
+
+    builder.addEntityProvider(
+        LDAPImmobiliare.fromConfig(env.config, {
+            id: 'admin',
+            target: 'ldap://localhost:1389',
+            logger: env.logger,
+            schedule: env.scheduler.createScheduledTaskRunner({
+                frequency: {minutes: 60},
+                timeout: {minutes: 15},
+            }),
+        }),
+    );
+
+    builder.addProcessor(new ScaffolderEntitiesProcessor());
+    const {processingEngine, router} = await builder.build();
+    await processingEngine.start();
+    return router;
 }
