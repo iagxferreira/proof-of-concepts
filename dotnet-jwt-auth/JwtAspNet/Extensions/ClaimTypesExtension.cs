12a3d47f7df7d5dd611a7e5f4284c18ee7071b8a
Iago Ferreira <iago-ferreira@outlook.com>
2023-09-15 17:25:39 -0300

    feat: criando extension para refatoração de claims

---
+++ dotnet-jwt-auth/JwtAspNet/Extensions/ClaimTypesExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace JwtAspNet.Extensions;
+
+public static class ClaimTypesExtension
+{
+    public static int Id(this ClaimsPrincipal user){
+        try
+        {
+            var id = user.Claims.FirstOrDefault(x => x.Type == "id").Value ?? "0";
+            return int.Parse(id);
+        }
+        catch
+        {
+            
+            return 0;
+        }
+    }
+}
--- dotnet-jwt-auth/JwtAspNet/Program.cs
+++ dotnet-jwt-auth/JwtAspNet/Program.cs
@@ -1,17 +1,52 @@
+using System.Security.Claims;
+using System.Text;
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<TokenService>();
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+
+builder.Services.AddAuthentication( options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("admin", policy => policy.RequireRole("admin"));
+});
 
 var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
 
-app.MapGet("/", (TokenService service) => {
+app.MapGet("/login", (TokenService service) =>
+{
     return service.Create(new User(1, "Iago Ferreira", "iago-ferreira@outlook.com", "teste.png", "password", new[] { "admin", "client" }));
 });
+
+app.MapGet("/restrito", (ClaimsPrincipal user) => {
+    return new {
+            id = user.Claims.FirstOrDefault(x => x.Type == "id").Value,
+            name = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value,
+            email = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value,
+            givenName = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName).Value,
+            image = user.Claims.FirstOrDefault(x => x.Type == "image").Value,
+        };
+    })
+    .RequireAuthorization();
+    
+app.MapGet("/admin", () => "Você tem acesso")
+    .RequireAuthorization("admin");
+
 app.Run();
