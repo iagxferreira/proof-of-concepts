5e26d3c0731c9470e62a5dd8d55cd15eeef73e80
Iago Ferreira <iago-ferreira@outlook.com>
2023-10-04 03:26:10 -0300

    feat: added models student and premium

---
+++ dotnet-razor/RazorTest/Models/Premium.cs
@@ -0,0 +1,31 @@
+﻿using RazorTest.Models;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Premium
+{
+    [Key]
+    [DisplayName("id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name shouldn't be null")]
+    [StringLength(80, ErrorMessage = "Name shouldn't have more than 80 chars")]
+    [MinLength(5, ErrorMessage = "Name should have more than 5 chars")]
+    [DisplayName("Fullname")]
+    public string Name { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    //[GreaterThanToday]
+    [DisplayName("Start")]
+    public DateTime StartDate { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("End")]
+    public DateTime EndDate { get; set; }
+
+    [DisplayName("Student")]
+    [Required(ErrorMessage = "Invalid student")]
+    public int StudentId { get; set; }
+}
+
---
+++ dotnet-razor/RazorTest/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using RazorTest.Models;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Student
+{
+    [Key]
+    [DisplayName("id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="Name shouldn't be null")]
+    [StringLength(80, ErrorMessage ="Name shouldn't have more than 80 chars")]
+    [MinLength(5, ErrorMessage ="Name should have more than 5 chars")]
+    [DisplayName("Fullname")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email shouldn't be null")]
+    [EmailAddress(ErrorMessage = "Invalid e-mail")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;
+
+    public List<Premium> Premiums { get; set; } = new();
+}
