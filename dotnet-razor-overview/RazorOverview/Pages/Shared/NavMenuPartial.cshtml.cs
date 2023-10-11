d2db9b5820f2ee73a811d336a5b473dd5b72419c
Iago Ferreira <iago-ferreira@outlook.com>
2023-10-11 03:21:59 -0300

    feat: added razor pages base project

---
+++ dotnet-razor-overview/RazorOverview/Pages/Shared/NavMenuPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorOverview.Pages.Shared
+{
+    public class NaveMenuPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ dotnet-razor-overview/RazorOverview/Pages/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorOverview.Pages
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
