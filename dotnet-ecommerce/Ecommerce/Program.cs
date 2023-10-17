0f0a77e2a5d05afff67dcb2fc0991122a96751cb
Iago Ferreira <iago-ferreira@outlook.com>
2023-10-17 00:55:07 -0300

    feat: added base project

---
+++ dotnet-ecommerce/Ecommerce/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace LanchesMac
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //criamos o m�todo de extens�o CreateAdminRole
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
