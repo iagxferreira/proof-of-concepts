170b2cdf34fe179b00a4bd8010f5005888cb941c
Iago Ferreira <iago-ferreira@outlook.com>
2023-11-17 20:28:32 -0300

    feat: add source folder

--- rust_rocket/src/main.rs
+++ rust_rocket/src/main.rs
@@ -1,3 +1,64 @@
+// main.rs
+
+mod entities;
+use futures::executor::block_on;
+use sea_orm::*;
+use entities::{prelude::*, *};
+// Change this according to your database implementation,
+// or supply it as an environment variable.
+// the whole database URL string follows the following format:
+// "protocol://username:password@host:port/database"
+// We put the database name (that last bit) in a separate variable simply for convenience.
+const DATABASE_URL: &str = "postgres://postgres:password@localhost:15432";
+const DB_NAME: &str = "bakeries_db";
+
+async fn run_create_table() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+
+    let _db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
+
+    Ok(())
+}
+
+async fn run() -> Result<(), DbErr> {
+    let happy_bakery = bakery::ActiveModel {
+        name: ActiveValue::Set("Happy Bakery".to_owned()),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+    let res = Bakery::insert(happy_bakery).exec(db).await?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    }
 }
