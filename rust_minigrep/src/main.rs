77a7b9dad963785bdda9c5547aefbd0f247fc040
Iago Ferreira <iago-ferreira@outlook.com>
2023-11-19 00:18:59 -0300

    feat: added parsing functions

---
+++ rust_minigrep/src/main.rs
@@ -0,0 +1,27 @@
+use std::{env, process};
+use std::error::Error;
+use rust_minigrep::{Config, run};
+
+mod lib;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+
+
+
